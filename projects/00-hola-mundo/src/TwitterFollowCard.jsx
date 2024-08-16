78ccb2fcb3a39ce09193c09e66d64f991557d3e0
LuisCorreaDv <dev.luis.correa@gmail.com>
2024-08-15 18:49:45 -0600

    Uso de useState para manejar el follow

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,9 +1,14 @@
-export function TwitterFollowCard ({children,userName, isFollowing}) {
-    
+import { useState } from "react";
+export function TwitterFollowCard ({children,userName}) {
+    const [isFollowing, setIsFollowing] = useState(false)
     const text = isFollowing ? 'Siguiendo' : 'Seguir';
     const buttonClassName = isFollowing 
     ? 'tw-followCard-button is-following' 
     : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing);
+    }
 
     return (
         <article className='tw-followCard'>
@@ -22,7 +27,7 @@ export function TwitterFollowCard ({children,userName, isFollowing}) {
             </header>
 
             <aside>
-                <button className={buttonClassName}>
+                <button className={buttonClassName} onClick={handleClick}>
                     {text}
                 </button>
             </aside>
