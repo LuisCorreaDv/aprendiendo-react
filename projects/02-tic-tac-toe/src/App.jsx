b6954175deab33a0db59ab64fc14da2c6d4e17ea
LuisCorreaDv <dev.luis.correa@gmail.com>
2024-08-20 19:28:14 -0600

    Añadiendo un renderizado condicional para visualizar el turno de cada jugador

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,31 +1,40 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 const TURNS = {
-  X: 'x',
-  O: 'o'
-}
+  X: "x",
+  O: "o",
+};
 
-const board = Array(9).fill(null);
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ""}`
+  return <div className={className}>{children}</div>;
+};
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
+
   return (
-    <main className='board'>
+    <main className="board">
       <h1>Tic tac toe</h1>
-      <section className='game'>
-        {
-          board.map((_, index) => {
-            return (
-              <div className='cell' key={index}>
-                <span className='cell__content'>
-                  {index}
-                </span>
-              </div>
-            )
-          })
-        }
+      <section className="game">
+        {board.map((_, index) => {
+          return <Square key={index}>{index}</Square>;
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
