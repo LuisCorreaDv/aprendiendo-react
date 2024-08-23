79c11b48d369413562f573498a616794ffa22214
LuisCorreaDv <dev.luis.correa@gmail.com>
2024-08-22 19:47:59 -0600

    Creando el estado para definir un ganador

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -17,20 +17,50 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
     </div>;
 };
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(TURNS.X);
 
+  //null es que no hay ganador, false es que hay empate
+  const [winner, setWinner] = useState(null) 
+
+  const checkWinner = (boardToCheck) => {
+    //Checar todas las combinaciones ganadoras
+    //para ver si X u O ganó 
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck [a] && //0 -> X u O
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a] // X u O
+      }
+    } 
+    //No hay ganador
+    return null
+  }
+
   const updateBoard = (index) => {
 
     //No actualizas la posicion si ya tiene algo 
-    if(board[index]) return
+    if(board[index] || winner) return
 
     /*Hacer una copia del tablero para actualizarlo ya que no se debe mutar el estado directamente. 
     Siempre deben ser nuevos los datos del renderizado
     */
-   
     const newBoard = [...board]
     //Pasar el valor del turno a la opcion que seleccionó el usuario
     newBoard[index] = turn
@@ -40,6 +70,12 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     //Actualizar el turno
     setTurn(newTurn)
+
+    //Revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
