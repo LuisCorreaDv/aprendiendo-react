2be2fb7e002fee9c37dbf1c7355a4b57c45e752e
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-04 18:52:31 -0600

    Paso 1 - Recuperar un hecho aleatorio de la primera API

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react"
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+
 export function App () {
+
+    const [fact, setFact] = useState('Cat fact')
+
+    //1) Fetch a la API de gatitos para obtener un hecho
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(response => response.json())
+        .then(data => setFact(data.fact))
+    },[])
+
     return (
-        <h1>App de gatitos</h1>
+        <main>
+            <h1>App de gatitos</h1>
+            <p>{fact}</p>
+        </main>
     )
 }
