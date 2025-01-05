251c507e0b17eabbb9fd4b8173024ca5911ad8a6
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-04 18:58:39 -0600

    Añadiendo renderizado condicional

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -16,7 +16,9 @@ export function App () {
     return (
         <main>
             <h1>App de gatitos</h1>
-            <p>{fact}</p>
+
+            {/* Renderizado condicional */}
+            {fact && <p>{fact}</p>}
         </main>
     )
 }
