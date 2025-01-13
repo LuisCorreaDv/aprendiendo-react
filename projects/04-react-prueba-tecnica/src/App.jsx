527c77f73f715ed9ecefed54a3aed389acfd09eb
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-13 16:35:17 -0600

    Usando un segundo efecto para dividir responsabilidades

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -11,26 +11,31 @@ export function App () {
 
     //1) Fetch a la API de gatitos para obtener un hecho
     useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(response => response.json())
-            .then(data => {
-                const {fact} = data
-                setFact(fact)
+      fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then((response) => response.json())
+        .then((data) => {
+          const { fact } = data;
+          setFact(fact);
+        });
+    }, []);
 
-                //2)Recuperar las 3 primeras palabras del hecho 
-                const threeFirstWords = fact.split(' ',3).join(' ')
-                
-                //3) Fetch a la API de gatitos para obtener la imagen
-                 fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-                     .then(response => response.json())
-                     .then(response => {
-                
-                        const { _id } = response
-                        const url = `https://cataas.com/cat/${_id}/says/${threeFirstWords}?size=50&color=red`
-                         setImageUrl(url)
-                    });
-            })
-    },[])
+    useEffect(() => {
+        if (!fact) return;
+        
+      //2)Recuperar las 3 primeras palabras del hecho
+      const threeFirstWords = fact.split(" ", 3).join(" ");
+
+      //3) Fetch a la API de gatitos para obtener la imagen
+      fetch(
+        `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          const { _id } = response;
+          const url = `https://cataas.com/cat/${_id}/says/${threeFirstWords}?size=50&color=red`;
+          setImageUrl(url);
+        });
+    }, [fact]);
 
     return (
         <main>
