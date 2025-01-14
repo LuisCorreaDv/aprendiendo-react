54dae01d724203f6ab76712b5a6acc05fa8ce487
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-14 17:57:49 -0600

    Configuraciones de Playwright y test del programa

--- projects/04-react-prueba-tecnica/tests/example.spec.js
+++ projects/04-react-prueba-tecnica/tests/example.spec.js
@@ -1,19 +1,20 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+//Se puede configurar con variables de entorno
+const LOCAL_HOST_URL = 'http://localhost:5173/'
+const CAT_PREFIX_URL = 'https://cataas.com'
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+//Los test funcionan con promesas
+test('app shows random fact and image', async ({ page }) => {
+  await page.goto(LOCAL_HOST_URL);
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  const text = await page.getByRole('paragraph')
+  const image = await page.getByRole('img')
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const textContent = await text.textContent()
+  const imageSrc = await image.getAttribute('src')
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(textContent?.length).toBeGreaterThan(0);
+  await expect(imageSrc?.startsWith(CAT_PREFIX_URL)).toBeTruthy();
 });
