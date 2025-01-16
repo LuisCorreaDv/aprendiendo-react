2c72a01db2d09f9b8ae94e138708e24d4382d385
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-16 17:15:19 -0600

    Uso del mock para renderizar las peliculas

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,7 +1,12 @@
 
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies.length > 0;
+
   return (
     <div className='page'>
       <header>
@@ -13,7 +18,25 @@ function App() {
       </header>
 
       <main>
-        Aqui van los resultados
+        {/* Renderizado condicional  */}
+        {
+          hasMovies ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              }
+            </ul>
+          )
+          : (
+            <p>No se encontraron peliculas para esta busqueda</p>
+          )
+        }
       </main>
     </div>
   );
