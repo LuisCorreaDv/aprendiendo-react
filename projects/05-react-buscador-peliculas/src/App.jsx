3500b99dbecee60824bff0118cd71862bdb50132
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-17 16:20:25 -0600

    useRef para identificar si es la primera vez que el usuario usa el input

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,23 +1,32 @@
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function useSearch() {
-  const [search, updateSearch] = useState("");
+  const [search, updateSearch] = useState('');
   const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
   useEffect(() => {
+    if(isFirstInput.current) {
+      isFirstInput.current = search === '';
+      return
+    }
+    
     if (search === "") {
       setError("No se puede buscar una película vacía");
+      return
     }
 
     if (search.match(/[^A-Za-z0-9\s]/)) {
       setError("No se pueden introducir caracteres especiales");
+      return
     }
 
     if (search.length < 3) {
       setError("La búsqueda debe tener al menos 3 caracteres");
+      return
     }
 
     setError(null);
