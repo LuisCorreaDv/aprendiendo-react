e37f65fcc832dc2262ca91169774728ae4bddc75
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-16 18:29:28 -0600

    Usando FormData para recuperar los datos del formulario

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -3,21 +3,29 @@ import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 
 function App() {
+  const { movies } = useMovies();
 
-  const { movies:mappedMovies } = useMovies();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //const fields = Object.fromEntries(new window.FormData(event.target)); -> Para recuperar los datos de un formulario con muchos inputs
+    const fields = new window.FormData(event.target);
+    const query = fields.get("query");
+    console.log(query);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form action="" className="form">
-          <input type="text" placeholder="Movie to Search..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input name="query" type="text" placeholder="Movie to Search..." />
           <button type="submit">Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
