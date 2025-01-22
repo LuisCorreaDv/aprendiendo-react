1d6335ade9d815d670b31158fca8537279c7da37
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-22 17:13:51 -0600

    Ordenar películas por orden alfabético

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -36,9 +36,9 @@ function useSearch() {
 }
 
 function App() {
-  
+  const [sort,setSort] = useState(false);
   const { search, updateSearch, error } = useSearch();
-  const { movies, getMovies, loading } = useMovies({ search });
+  const { movies, getMovies, loading } = useMovies({ search, sort });
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -53,6 +53,10 @@ function App() {
     updateSearch(event.target.value);
   };
 
+  const handleSort = () => {
+    setSort(!sort);
+  }
+
   return (
     <div className="page">
       <header>
@@ -65,6 +69,7 @@ function App() {
             type="text"
             placeholder="Movie to Search..."
           />
+          <input type="checkbox" onChange={handleSort} checked={sort}/>
           <button type="submit">Buscar</button>
         </form>
         {error && <p style={{ color: "red" }}>{error}</p>}
