232c376925b60fe84762af2716cf56dfc24dff4a
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-22 18:13:36 -0600

    useCallback para simplificar sintaxis al estar trabajando con funciones

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useMemo } from "react";
+import { useRef, useState, useMemo, useCallback } from "react";
 import { searchMovies } from "../services/movies";
 
 export function useMovies({ search, sort }) {
@@ -11,35 +11,32 @@ export function useMovies({ search, sort }) {
 
   //Buscar las peliculas con el input
   //useMemo para que no se ejecute en cada renderizado
-  const getMovies = useMemo(() => {
-    //
-    return async ({search}) => {
-      //Si la búsqueda es igual a la anterior, no se hace nada
-      if (search === previousSearch.current) return;
 
-      try {
-        setLoading(true);
-        setError(null);
+  //useCallback para que no se cree una nueva función en cada renderizado
+  const getMovies = useCallback(async ({ search }) => {
+    //Si la búsqueda es igual a la anterior, no se hace nada
+    if (search === previousSearch.current) return;
 
-        //Guardamos la búsqueda actual
-        previousSearch.current = search;
-        const newMovies = await searchMovies({ search });
-        setMovies(newMovies);
-      } catch (e) {
-        setError(e.message);
-      } finally {
-        //Se ejecuta siempre, tanto si hay error como si no
-        setLoading(false);
-      }
-    };
-    //Si cambia la búsqueda, se ejecuta de nuevo
+    try {
+      setLoading(true);
+      setError(null);
+
+      //Guardamos la búsqueda actual
+      previousSearch.current = search;
+      const newMovies = await searchMovies({ search });
+      setMovies(newMovies);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      //Se ejecuta siempre, tanto si hay error como si no
+      setLoading(false);
+    }
   }, []);
 
   const sortedMovies = useMemo(() => {
-
     return sort
-    ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
-    : movies;
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
   }, [sort, movies]);
 
   return { movies: sortedMovies, getMovies, loading, error };
