3e70e1a27e69e74b550fc22040ece20790cc1393
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-01-22 19:08:04 -0600

    Aplicar debounce al realizar la búsqueda

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
+import debounce from 'just-debounce-it';
 
 function useSearch() {
   const [search, updateSearch] = useState('');
@@ -40,6 +41,13 @@ function App() {
   const { search, updateSearch, error } = useSearch();
   const { movies, getMovies, loading } = useMovies({ search, sort });
 
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      getMovies({ search });
+    },300)
+    ,[getMovies]
+  )
+  
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -52,7 +60,7 @@ function App() {
   const handleChange = (event) => {
     const newSearch = event.target.value
     updateSearch(newSearch);
-    getMovies({search: newSearch})
+    debouncedGetMovies({newSearch});
   };
 
   const handleSort = () => {
