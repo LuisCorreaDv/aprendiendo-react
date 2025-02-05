7ac9e4f896048eddcf09dfd28d61b314766c223f
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-05 17:08:54 -0600

    Mejorando estilos, nuevo formato json y error en componente de productos

---
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -0,0 +1,29 @@
+import "./Products.css";
+import { AddToCartIcon } from "./Icons.jsx";
+
+// eslint-disable-next-line react/prop-types
+export function Products({ products }) {
+    
+  return (
+    <main className="products">
+      <ul>
+        {/* eslint-disable-next-line react/prop-types */}
+        {products.map(product => {
+        return (
+            <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <div>
+                <strong>{product.title}</strong>
+            </div>
+            <div>
+                <button>
+                    <AddToCartIcon />
+                </button>
+            </div>
+          </li>
+        )
+        })}
+      </ul>
+    </main>
+  )
+}
