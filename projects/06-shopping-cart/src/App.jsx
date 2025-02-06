a79675a000c8de710048348f86be0f1147cb3fd1
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-05 18:30:25 -0600

    Filtro de productos

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,10 +1,34 @@
-import { products } from './mocks/products.json'
+import { products as initialProducts} from './mocks/products.json'
 import { Products } from './components/Products'
+import { useState } from 'react'
 
-function App() {
+function App() { 
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0 
+  })
 
+  // Función para filtrar los productos
+  const filterProducts = (products) => {
+    // Filter para regresar los productos que cumplan con las condiciones
+    return products.filter(product => {
+      
+      return (
+        // Si el precio del producto es mayor o igual al precio mínimo y  la categoría del producto es igual a la categoría seleccionada o la categoría seleccionada es 'all'
+        product.price >= filters.minPrice && 
+        (
+          filters.category === 'all' || 
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+  
   return (
-    <Products products={products}/>
+    <Products products={filteredProducts}/>
   )
 }
 
