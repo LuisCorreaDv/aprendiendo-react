ae32c67b8af5153c275f140b46435c4ce6272d11
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-05 18:52:43 -0600

    Mostrando el cambio de precio con el rango

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,18 +1,33 @@
-import './Filters.css'
+import { useState } from "react";
+import "./Filters.css";
 
 export function Filters() {
+  const [minPrice, setMinPrice] = useState(0);
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value)
+  }
+
   return (
     <section className="filters">
       <div>
-        <label htmlFor="price">Price</label>
-        <input type="range" name="price" id="price" min="0" max="1000" />
+        <label htmlFor="price">Precio a partir de: </label>
+        <input 
+            type="range" 
+            name="price" 
+            id="price" 
+            min="0" 
+            max="1000" 
+            onChange={handleChangeMinPrice}
+        />
+        <span>${minPrice}</span>
       </div>
       <div>
         <label htmlFor="category">Categoria</label>
         <select name="category" id="category">
-            <option value="all">Todas</option>
-            <option value="fragrances">Perfumes</option>
-            <option value="furniture">Muebles</option>
+          <option value="all">Todas</option>
+          <option value="fragrances">Perfumes</option>
+          <option value="furniture">Muebles</option>
         </select>
       </div>
     </section>
