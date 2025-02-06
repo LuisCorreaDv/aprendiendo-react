8de88e6da2a1d3cbedf66281b99e9d8d73842208
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-06 15:40:25 -0600

    Custom hook para la funcionalidad de los filtros

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,37 +3,40 @@ import { Products } from './components/Products'
 import { useState } from 'react'
 import { Header } from './components/Header'
 
-function App() { 
-  const [products] = useState(initialProducts)
+// Custom hook que se encarga de la funcionalidad de los filtros
+function useFilters () {
   const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0 
-  })
+    category: "all",
+    minPrice: 0,
+  });
 
   // Función para filtrar los productos
   const filterProducts = (products) => {
     // Filter para regresar los productos que cumplan con las condiciones
-    return products.filter(product => {
-      
+    return products.filter((product) => {
       return (
         // Si el precio del producto es mayor o igual al precio mínimo y  la categoría del producto es igual a la categoría seleccionada o la categoría seleccionada es 'all'
-        product.price >= filters.minPrice && 
-        (
-          filters.category === 'all' || 
-          product.category === filters.category
-        )
-      )
-    })
-  }
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
 
-  const filteredProducts = filterProducts(products)
-  
+  return {filterProducts, setFilters}
+}
+
+function App() {
+  const [products] = useState(initialProducts);
+  const { filterProducts, setFilters } = useFilters();
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
-      <Header changeFilters={setFilters}/>
-      <Products products={filteredProducts}/>
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
     </>
-  )
+  );
 }
 
 export default App
