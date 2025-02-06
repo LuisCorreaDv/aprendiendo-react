0d4dc28ddd07d20a6b17ffa4b4e856c35473224b
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-05 19:07:38 -0600

    Prop drilling para el funcionamiento de los filtros

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import "./Filters.css";
 
-export function Filters() {
+// eslint-disable-next-line react/prop-types
+export function Filters({onChange}) {
   const [minPrice, setMinPrice] = useState(0);
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value)
+    // DOS FUENTES DE LA VERDAD
+    //Se sa prevState para obtener el estado anterior y no perder la referencia 
+    onChange(prevState => ({
+        ...prevState,
+        minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+
+    //Se esta pasando la funcion de actualizacion de estado nativa de react a un componente hijo
+    onChange(prevState => ({
+        ...prevState,
+        category: event.target.value
+    }))
   }
 
   return (
@@ -24,7 +40,7 @@ export function Filters() {
       </div>
       <div>
         <label htmlFor="category">Categoria</label>
-        <select name="category" id="category">
+        <select name="category" id="category" onChange={handleChangeCategory}>
           <option value="all">Todas</option>
           <option value="fragrances">Perfumes</option>
           <option value="furniture">Muebles</option>
