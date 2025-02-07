fd20fdfe430465b85f116d76f3e377bd53be3b4e
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-07 16:13:08 -0600

    Quitando el prop drilling del footer

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -7,7 +7,7 @@ import { useFilters } from './hooks/useFilters.js'
 
 function App() {
   const [products] = useState(initialProducts);
-  const { filters, filterProducts } = useFilters();
+  const { filterProducts } = useFilters();
 
   const filteredProducts = filterProducts(products);
 
@@ -15,7 +15,7 @@ function App() {
     <>
       <Header/>
       <Products products={filteredProducts} />
-      <Footer filters={filters}/>
+      <Footer/>
     </>
   );
 }
