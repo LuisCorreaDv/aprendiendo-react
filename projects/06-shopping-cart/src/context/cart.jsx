bd8c9a81d226b939086b4bb6d5db262adc430031
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-07 17:12:41 -0600

    Creando un contexto para manejar el carrito de compras

---
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+// 1. Crear el contexto
+export const CartContext = createContext()
+
+//2. Crear el provider del contexto
+// eslint-disable-next-line react/prop-types
+export function CartProvider ({children}) {
+    const [cart, setCart] = useState([])
+
+    const addToCart = product =>{}
+    const clearCart = () => {
+        setCart([])
+    }
+
+    return (
+        <CartContext.Provider value={{
+            cart,
+            addToCart,
+            clearCart
+        }}
+        >
+            {children}
+        </CartContext.Provider>
+    )
+}
+
