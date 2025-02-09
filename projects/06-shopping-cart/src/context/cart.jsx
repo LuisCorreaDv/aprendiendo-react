dd0c8e45b91952a042e69e507b9a18269dab2e69
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-02-08 18:17:39 -0600

    Funcionalidad para agregar productos al carrito y aumentar la cantidad si ya está seleccionado

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -9,8 +9,27 @@ export function CartProvider ({children}) {
     const [cart, setCart] = useState([])
 
     const addToCart = product =>{
-        //Forma sencilla de añadir un producto al carrito
-        setCart([...cart, product])
+        //Checar si el producto ya esta en el carrito
+        const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+        // Si hay un producto en el carrito
+        if (productInCartIndex >= 0) {
+            // Se usa structuredClone para hacer una copia del carrito y no modificar el original
+            const newCart = structuredClone(cart)
+            // Se aumenta la cantidad del producto en el carrito
+            newCart[productInCartIndex].quantity += 1
+            // Se actualiza el carrito
+            return setCart(newCart)
+        }
+
+        // Si no hay un producto en el carrito, se agrega con el estado anterior y se le asigna una cantidad de 1
+        setCart(prevState => {[
+            ...prevState,
+            {
+                ...product,
+                quantity: 1
+            }
+        ]})
     }
 
     const clearCart = () => {
